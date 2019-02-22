ecb53c41b62e64db1ac850d55634eac1c147e53f
ditam <ditam@users.noreply.github.com>
2019-02-22 01:03:17 +0100

    add result validator to temp/keno

--- temp/keno/keno.js
+++ temp/keno/keno.js
@@ -4,6 +4,12 @@ $('#warning').hide();
 var TIMEOUT = 60 * 1000;
 var rowWorker;
 
+function parseNumberArrayString(numberListString) {
+  return numberListString.split(',').map(function(numberAsString) {
+    return parseInt(numberAsString, 10);
+  });
+}
+
 // TODO: onchange of maxRepeat, bound to 1-numCount
 
 $('button#generate').click(function() {
@@ -12,7 +18,7 @@ $('button#generate').click(function() {
   var maxRepeat = $('input#maxRepeat').val();
   var rowCount = $('input#rowCount').val();
 
-  $('textarea').html('')
+  $('textarea').val('')
   
   var rows = [];
   rowWorker = new Worker('worker.js');
@@ -26,7 +32,7 @@ $('button#generate').click(function() {
     clearTimeout(timeout);
     var newRow = e.data;
     rows.push(newRow);
-    $('textarea').html($('textarea').html()+newRow+'\n');
+    $('textarea').val($('textarea').val()+newRow+'\n');
     
     if (rows.length < rowCount) {
       $('#warning').slideUp();
@@ -47,13 +53,45 @@ $('button#terminate').click(function() {
   $('#warning').slideUp();
 });
 
+$('button#validate').click(function() {
+  var results = parseNumberArrayString( $('input#results').val() );
+  var rows = $('textarea').val().split('\n').filter(function(rowAsString) {
+    return rowAsString !== '';
+  }).map(function(rowAsString) {
+    return parseNumberArrayString(rowAsString);
+  });
+  var numCount = rows[0].length;
+  
+  var resultCounter = {};
+  for (var i=0;i<=numCount;i++) {
+    resultCounter[i] = 0; 
+  }
+  
+  rows.forEach(function(row) {
+    var hits = 0;
+    row.forEach(function(number) {
+      if (results.includes(number)) {
+        hits++;
+      }
+    });
+    resultCounter[hits]++;
+  });
+  
+  var output = 'Eredmények:\n===========\n\n';
+  for (var key in resultCounter) {
+    output += key + ' találat: ' + resultCounter[key] + '\n';
+  }
+  
+  $('textarea').val(output)
+});
+
 $('button#download').click(function() {
   var numCount = $('input#numCount').val();
   var maxRepeat = $('input#maxRepeat').val();
   var rowCount = $('input#rowCount').val();
   
   // via https://stackoverflow.com/a/19332584
-  var textToWrite = $('textarea').html();
+  var textToWrite = $('textarea').val();
   var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
   var fileNameToSaveAs = 'Keno_' + numCount + 'szam_' + maxRepeat + 'ismetles_' + rowCount + '_szelveny.csv';
 
